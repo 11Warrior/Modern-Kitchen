8417d7fa6220bb3eaaf22b455d7630c879ab7b68
11Warrior <aayushthapa640@gmail.com>
2026-04-09 23:31:03 +0530

    Some description tweaks in the p tags

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,7 +2,6 @@
 import AdminDashboard from '@/components/Admin/AdminDashboard';
 import { currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
-import React from 'react'
 
 const Dashboard = async () => {
   const user = await currentUser();
